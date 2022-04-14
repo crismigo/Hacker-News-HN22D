9c606f086c79f3e3d5e11638b7ea41b74c198e99
Àlex Ollé <alex.olle@estudiantat.upc.edu>
2022-04-14 17:59:52 +0200

    task #28: Segona versió de la tempalte base de la pàgina

--- HackerNews/news/urls.py
+++ HackerNews/news/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.news,name="News"),
+    path('', views.news,name="Home"),
     path('news/', views.news,name="News"),
     path('newest/', views.newest,name="Newest"),
     path('ask/', views.ask,name="Ask"),
