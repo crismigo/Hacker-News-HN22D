8f6f337e279c00af6e1389b722f36143492b2f16
crismigo <cristinamigo01@gmail.com>
2022-04-18 01:04:05 +0200

    task #39: - html de la profile.html view feta, - funcio per created at creada al models.py de authentication, - forms.py creat per profileApp - style.css creat de la pagina profile.html

--- HackerNews/userProfile/views.py
+++ HackerNews/userProfile/views.py
@@ -1,14 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+from authentication.models import User
+from userProfile.forms import UserForm
+
+
 def show(request, user_id):
+    if request.user.is_authenticated:
+        user_form = UserForm()
+        user = User.objects.get(id=user_id)
+        user.timeSinceCreation()
+        return render(request, "profile.html", {"form": user_form, "user": user})
+
+    else:
+        return redirect("/login")
+
+
+def submissions(request, user_id):
     pass
 
-def submissions(request,user_id):
-    pass
 
 def upvoted_submissions(request):
     pass
+
 
 def upvoted_comments(request):
     pass
