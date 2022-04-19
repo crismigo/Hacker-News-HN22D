3fe38c676c7ef57535fb73543132d80166b2804c
Àlex Ollé <alex.olle@estudiantat.upc.edu>
2022-04-19 19:57:09 +0200

    task #52: ja es poden afegir comentaris a un comentari (reply)

--- HackerNews/comment/urls.py
+++ HackerNews/comment/urls.py
@@ -4,10 +4,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('submission/add/<int:id_submission>', views.addSubmissionComment, name="AddSubmissionComment"),
-    path('submission/edit', views.editSubmissionComment, name="EditSubmissionComment"),
-    path('submission/remove', views.removeSubmissionComment, name="RemoveSubmissionComment"),
-    path('comment/add', views.addReplyComment, name="AddComment2Comment"),
-    path('comment/edit', views.editReplyComment, name="EditComment2Comment"),
-    path('comment/remove', views.removeReplyComment, name="RemoveComment2Comment"),
+    path('submission/<int:id_submission>', views.addSubmissionComment, name="AddSubmissionComment"),
+    path('reply/<int:id_comment>', views.addReplyComment, name="AddComment2Comment"),
 ]
