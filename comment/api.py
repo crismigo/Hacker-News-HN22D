78f56a0441efe45632457a2e748f423ad41269db
crismigo <cristinamigo01@gmail.com>
2022-05-01 18:40:49 +0200

    task #76: fitxers urls-api.py i api.py de comments modificats per al funcionament del metode post

--- comment/api.py
+++ comment/api.py
@@ -5,7 +5,7 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from comment.models import Comment
+from comment.models import Comment, ActionType
 from comment.serializers import CommentSerializer
 
 
@@ -27,9 +27,36 @@ class CommentDetailApiView(APIView):
         serializer = CommentSerializer(comment_instance)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
+
+class CommentToCommentDetailApiView(APIView):
     def post(self, request):
+        comment_type = ActionType.objects.get(name="Comment")
         data = {
-            'type': request.data.get('type'),
+            'type': comment_type,
             'user': request.data.get('user'),
             'text': request.data.get('text'),
+            'replied_comment': request.data.get('replied_comment')
         }
+        serializer = CommentSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CommentToSubmissionDetailApiView(APIView):
+    def post(self, request):
+        submission_type = ActionType.objects.get(name="Submission")
+        data = {
+            'type': submission_type,
+            'user': request.data.get('user'),
+            'text': request.data.get('text'),
+            'submission': request.data.get('submission')
+        }
+        serializer = CommentSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- comment/urls-api.py
+++ comment/urls-api.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
-from .api import CommentDetailApiView
+from .api import CommentDetailApiView, CommentToSubmissionDetailApiView, CommentToCommentDetailApiView
 
 urlpatterns = [
     path('<int:comment_id>/', CommentDetailApiView.as_view()),
+    path('reply/', CommentToCommentDetailApiView.as_view()),
+    path('submission/', CommentToSubmissionDetailApiView.as_view())
 ]
