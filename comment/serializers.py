32b27775fcbb2b659e97b8ca5ef881eb9c52c764
Àlex Ollé <alex.olle@estudiantat.upc.edu>
2022-05-03 21:19:25 +0200

    Tasca #75: S'ha fet que es retornin recursivament els comentaris i votes d'una news

--- comment/serializers.py
+++ comment/serializers.py
@@ -1,8 +1,28 @@
 from rest_framework import serializers
 from comment.models import Comment
+from vote.models import Vote
+from vote.serializers import VoteSerializer
 
 
 class CommentSerializer(serializers.ModelSerializer):
+    comments = serializers.SerializerMethodField("getComments")
+    votes = serializers.SerializerMethodField("getVotes")
+
+    def getVotes(self, parent):
+        comment_votes = Vote.objects.filter(comment_id=parent.id)
+        votes = []
+        for comm in comment_votes:
+            serializer = VoteSerializer(comm)
+            votes.append(serializer.data)
+        return votes
+
+    def getComments(self, parent):
+        comment_comm = Comment.objects.filter(replied_comment_id=parent.id)
+        comments = []
+        for comm in comment_comm:
+            serializer = CommentSerializer(comm)
+            comments.append(serializer.data)
+        return comments
     class Meta:
         model = Comment
         fields = ["id", "submission", "replied_comment", "type", "user", "text", "comments", "votes", "created_at"]
