9539d82ea57aee60aba4a7083801dd098838ba90
isslam.benali <isslam.benali@estudiantat.upc.edu>
2022-05-13 21:28:19 +0200

    Arreglat error Upvote. Faltava una / en la url-

--- vote/urls.py
+++ vote/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('voteSubmission/<int:id>', views.vote_submission, name="voteSubmission"),
     path('unvoteSubmission/<int:id>', views.unvote_submission, name="unvoteSubmission"),
     path('voteComment/<int:idc>', views.vote_comment, name="voteComment"),
-    path('unvoteComment<int:idc>/', views.unvote_comment, name="unvoteComment"),
+    path('unvoteComment/<int:idc>/', views.unvote_comment, name="unvoteComment"),
 ]
